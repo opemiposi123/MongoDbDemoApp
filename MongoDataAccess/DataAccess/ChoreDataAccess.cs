5e10fca83a17356fe2c30218c856b9945c33062b
AbdulWaheed MahmoodAhmad <abdulwaheedmahmoodahmad6@gmail.com>
2024-07-23 02:14:27 +0100

    Mogo Db

--- MongoDataAccess/DataAccess/ChoreDataAccess.cs
+++ MongoDataAccess/DataAccess/ChoreDataAccess.cs
@@ -4,7 +4,7 @@ using MongoDB.Driver.Core.Configuration;
 
 namespace MongoDataAccess.DataAccess;
 
-public  class ChoreDataAccess
+public class ChoreDataAccess
 {
 	private const string ConnectionString = "mongodb://localhost:27017";
 	private const string DatabaseName = "chore_db";
@@ -18,4 +18,62 @@ public  class ChoreDataAccess
 		var db = client.GetDatabase(DatabaseName);
 		return db.GetCollection<T>(collection);
 	}
+
+	public async Task<List<UserModel>> GetAllUsers()
+	{
+		var usersCollection = ConnectToMongo<UserModel>(UserCollection);
+		var results = await usersCollection.FindAsync(_ => true);
+		return results.ToList();
+	}
+
+	public async Task<List<ChoreModel>> GetAllChores()
+	{
+		var choresCollection = ConnectToMongo<ChoreModel>(ChoreCollection);
+		var results = await choresCollection.FindAsync(_ => true);
+		return results.ToList();
+	}
+
+	public async Task<List<ChoreModel>> GetAllChoresForAUser(UserModel user)
+	{
+		var choresCollection = ConnectToMongo<ChoreModel>(ChoreCollection);
+		var results = await choresCollection.FindAsync(c => c.AssignedTo.Id == user.Id);
+		return results.ToList();
+	}
+
+	public Task CreateUser(UserModel user)
+	{
+		var usersCollection = ConnectToMongo<UserModel>(UserCollection);
+		return usersCollection.InsertOneAsync(user);
+	}
+
+	public Task CreateChore(ChoreModel chore)
+	{
+		var choresCollection = ConnectToMongo<ChoreModel>(ChoreCollection);
+		return choresCollection.InsertOneAsync(chore);
+	}
+
+	public Task UpdateChore(ChoreModel chore)
+	{
+		var choresCollection = ConnectToMongo<ChoreModel>(ChoreCollection);
+		var filter = Builders<ChoreModel>.Filter.Eq("Id", chore.Id);
+		return choresCollection.ReplaceOneAsync(filter, chore, new ReplaceOptions { IsUpsert = true });
+	}
+
+	public Task DeleteChore(ChoreModel chore)
+	{
+		var choresCollection = ConnectToMongo<ChoreModel>(ChoreCollection);
+		return choresCollection.DeleteOneAsync(c => c.Id == chore.Id);
+	}
+
+	public async Task CompleChore(ChoreModel chore)
+	{
+		var choresCollection = ConnectToMongo<ChoreModel>(ChoreCollection);
+		var filter = Builders<ChoreModel>.Filter.Eq("Id", chore.Id);
+		await  choresCollection.ReplaceOneAsync(filter, chore);
+
+		var choreHistoryCollection = ConnectToMongo<ChoreHistoryModel>(ChoreHistoryCollection);
+		await choreHistoryCollection.InsertOneAsync(new ChoreHistoryModel(chore));
+	}
+
+
 }
--- MongoDbDemo/Program.cs
+++ MongoDbDemo/Program.cs
@@ -1,20 +1,47 @@
 ﻿using MongoDB.Driver;
-using MongoDbDemo; 
+using MongoDbDemo;
+using MongoDataAccess.DataAccess;
+using MongoDataAccess.Models;
 
-string connectionString = "mongodb://localhost:27017";
-string databaseName = "simple_db";
-string collectionName = "person";
-var client = new MongoClient(connectionString);
-var db = client.GetDatabase(databaseName);
-var collection = db.GetCollection<PersonModel>(collectionName);
-var person = new PersonModel
+//string connectionString = "mongodb://localhost:27017";
+//string databaseName = "simple_db";
+//string collectionName = "person";
+//var client = new MongoClient(connectionString);
+//var db = client.GetDatabase(databaseName);
+//var collection = db.GetCollection<PersonModel>(collectionName);
+//var person = new PersonModel
+//{
+//	FirstName = "DevMahmood",
+//	LastName = "AbdulWaheed"
+//};
+//await collection.InsertOneAsync(person);
+//var results = await collection.FindAsync(_ => true);
+//foreach (var result in results.ToList())
+//{
+//	Console.WriteLine($"{result.Id} {result.FirstName} {result.LastName}");
+//}
+
+
+ChoreDataAccess db = new ChoreDataAccess();
+await db.CreateUser(new UserModel()
 {
 	FirstName = "DevMahmood",
 	LastName = "AbdulWaheed"
-};
-await collection.InsertOneAsync(person);
-var results = await collection.FindAsync(_ => true);
-foreach (var result in results.ToList())
+});
+var users = await db.GetAllUsers();
+
+var chore = new ChoreModel()
 {
-	Console.WriteLine($"{result.Id} {result.FirstName} {result.LastName}");
-}
+	AssignedTo = users.First(),
+	ChoreText = "Learn New Things About C#",
+	FrequencyInDays = 1
+};
+await db.CreateChore(chore);
+
+var chores = await db.GetAllChores();
+
+var newChore = chores.First();
+
+newChore.LastCompleted = DateTime.UtcNow;
+
+await db.CompleChore(newChore);
